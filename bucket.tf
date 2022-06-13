2c60671908816edfaa7a4abb8d813c46c1984291
Vander Ribeiro Elme <vander@elme.eti.br>
2022-06-12 23:52:31 -0300

    inicio

---
+++ bucket.tf
@@ -0,0 +1,4 @@
+resource "aws_s3_bucket" "first_bucket" {
+    bucket = "exemplo-curso-terraform-cli"
+  
+}
---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = "~> 1.2.2" # 1.1.0 até 1.1.n
+
+  required_providers {
+    aws = {
+      version = ">=3.50.0"
+      source = "hashicorp/aws"
+    }
+    azurerm ={
+      version = "2.70.0"
+      source = "hashicorp/azurerm"
+    }
+ 
+  
+}
+}
